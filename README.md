d36888f1f19954bd8365d101cbc1930dbe143483
Daniel Cleveland <dpcleveland@gmail.com>
2016-09-12 21:30:25 -0500

    Initial setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# Show the Local Weather
+Show the Local Weather is an Intermediate Front End Development project from <a href="https://www.freecodecamp.com/challenges/show-the-local-weather">freeCodeCamp</a>.
+
+It fulfills the following user stories:
+1) I can see the weather in my current location.
+2) I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
+3) I can push a button to toggle between Fahrenheit and Celsius.
---
+++ dynamic/custom.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $("#new-quote").on("click", function(){
+        $.getJSON("/dynamic/json/quotes.json", function(json) {
+            var quoteText = "";
+            var attributeText = "";
+            var totalQuotes = json.length;
+            var newBackgroundColor = "";
+
+            function randomQuote() {
+                return Math.floor(Math.random() * (totalQuotes));
+            }
+            var chosenQuote = randomQuote();
+            console.log(chosenQuote);
+
+            json = json.filter(function(val) {
+                return (val.id == chosenQuote);
+            });
+
+            json.forEach(function(val) {
+                quoteText += "<i class='fa fa-quote-left' aria-hidden='true'></i> ";
+                quoteText += val.quote;
+                attributeText += val.attribute;
+                newBackgroundColor = val.color;
+            });
+
+            $("#quote-text").html(quoteText);
+            $("#attribution-text").html(attributeText);
+            $('body').css('background-color', newBackgroundColor);
+            $('.actions button').css('background-color', newBackgroundColor);
+            $('.social ul li').css('background', newBackgroundColor);
+        });
+    });
+});
