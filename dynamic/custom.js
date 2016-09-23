61a09c958d1a9460e886cd768a9893af66a9c97a
Daniel Cleveland <dpcleveland@gmail.com>
2016-09-22 22:18:06 -0500

    Get user weather conditions, added comments

--- dynamic/custom.js
+++ dynamic/custom.js
@@ -6,21 +6,33 @@ $(document).ready(function() {
     $.getJSON("//ip-api.com/json", function(json) {
         function navigator() {
 
+            // Pull user's location from http://ip-api.com/json
             var userLatitude = json.lat;
             console.log('Latitude : ' + userLatitude);
             var userLongitude = json.lon;
             console.log('Longitude: ' + userLongitude);
 
+            // Input user location into OpenWeatherMap API URL along with key
             var openWeatherMapURL = "http://api.openweathermap.org/data/2.5/weather?lat=" + userLatitude + "&lon=" + userLongitude + "&APPID=" + apiKey;
             console.log(openWeatherMapURL);
 
             $.getJSON(openWeatherMapURL, function(json) {
+
+                // Get Temperature & Unit Conversion
                 var userKelvinTemp = json.main.temp;
                 console.log(userKelvinTemp);
                 var userFahrenheitTemp = userKelvinTemp * (9/5) - 459.67;
                 console.log(userFahrenheitTemp);
                 var userCelsiusTemp = userKelvinTemp - 273.15;
                 console.log(userCelsiusTemp);
+
+                // Get Weather
+                var userWeatherCondition = json.weather[0].main;
+                console.log(userWeatherCondition);
+                var userWeatherIcon = json.weather[0].icon;
+                console.log(userWeatherIcon);
+                var userWeatherIconLink = "http://openweathermap.org/img/w/" + userWeatherIcon + ".png";
+
             });
         }
         navigator();
