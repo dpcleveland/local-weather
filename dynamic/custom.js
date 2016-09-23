9ec5ef3425e2fced6af301d1dd0b5d4f3961dac4
Daniel Cleveland <dpcleveland@gmail.com>
2016-09-22 22:04:34 -0500

    Switched from built in user locator to ip-api.com/json, added variables for user temperature

--- dynamic/custom.js
+++ dynamic/custom.js
@@ -3,50 +3,26 @@ $(document).ready(function() {
     // OpenWeatherMap API Key
     var apiKey = "96f6bcd552af365b9711c29cf4e5bdb4";
 
-    if (navigator.geolocation) {
-        var navigatorOptions = {
-            enableHighAccuracy: true,
-            timeout: 5000,
-            maximumAge: 0
-        };
+    $.getJSON("//ip-api.com/json", function(json) {
+        function navigator() {
 
-        function navigatorSuccess(pos) {
-            var crd = pos.coords;
-
-            var userLatitude = crd.latitude;
-            console.log('Latitude : ' + crd.latitude);
-            var userLongitude = crd.longitude;
-            console.log('Longitude: ' + crd.longitude);
+            var userLatitude = json.lat;
+            console.log('Latitude : ' + userLatitude);
+            var userLongitude = json.lon;
+            console.log('Longitude: ' + userLongitude);
 
             var openWeatherMapURL = "http://api.openweathermap.org/data/2.5/weather?lat=" + userLatitude + "&lon=" + userLongitude + "&APPID=" + apiKey;
             console.log(openWeatherMapURL);
 
             $.getJSON(openWeatherMapURL, function(json) {
-
-                json.forEach(function(val) {
-                    quoteText += "<i class='fa fa-quote-left' aria-hidden='true'></i> ";
-                    quoteText += val.quote;
-                    attributeText += val.attribute;
-                    newBackgroundColor = val.color;
-                });
-
-                $("#quote-text").html(quoteText);
-                $("#attribution-text").html(attributeText);
-                $('body').css('background-color', newBackgroundColor);
-                $('.actions button').css('background-color', newBackgroundColor);
-                $('.social ul li').css('background', newBackgroundColor);
+                var userKelvinTemp = json.main.temp;
+                console.log(userKelvinTemp);
+                var userFahrenheitTemp = userKelvinTemp * (9/5) - 459.67;
+                console.log(userFahrenheitTemp);
+                var userCelsiusTemp = userKelvinTemp - 273.15;
+                console.log(userCelsiusTemp);
             });
         }
-
-        function navigatorError(err) {
-            console.warn('ERROR(' + err.code + '): ' + err.message);
-        }
-
-        navigator.geolocation.getCurrentPosition(navigatorSuccess, navigatorError, navigatorOptions);
-
-
-    }
-    else {
-        // Tell user to turn on geo location
-    }
+        navigator();
+    });
 });
