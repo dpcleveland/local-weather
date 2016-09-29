4c8f87d41d5234e00c7c9f981a8c9d42932ce31f
Daniel Cleveland <dpcleveland@gmail.com>
2016-09-28 20:42:53 -0500

    Added error messages

--- dynamic/custom.js
+++ dynamic/custom.js
@@ -7,40 +7,56 @@ $(document).ready(function() {
     var userLatitude, userLongitude, openWeatherMapURL, userCity, userCountry, userKelvinTemp, userFahrenheitTemp, userCelsiusTemp, userWeatherCondition, userWeatherIcon, userWeatherIconLink, isCelcius = false;
 
     function getUserLocation() {
-        $.getJSON("//ip-api.com/json", function(json) {
-            // Pull user's location from http://ip-api.com/json
-			userLatitude = json.lat;
-			console.log('Latitude : ' + userLatitude);
-			userLongitude = json.lon;
-			console.log('Longitude: ' + userLongitude);
-            userCity = json.city;
-            console.log(userCity);
-            userCountry = json.countryCode;
-            console.log(userCountry);
+        $.getJSON("//ip-api.com/json", function(json, status) {
+            if (status == "success") {
+                // Pull user's location from http://ip-api.com/json
+			    userLatitude = json.lat;
+			    console.log('Latitude : ' + userLatitude);
+			    userLongitude = json.lon;
+			    console.log('Longitude: ' + userLongitude);
+                userCity = json.city;
+                console.log(userCity);
+                userCountry = json.countryCode;
+                console.log(userCountry);
 
-            // Input user location into OpenWeatherMap API URL along with key
-            openWeatherMapURL = "http://api.openweathermap.org/data/2.5/weather?lat=" + userLatitude + "&lon=" + userLongitude + "&APPID=" + apiKey;
-			console.log(openWeatherMapURL);
+                // Input user location into OpenWeatherMap API URL along with key
+                openWeatherMapURL = "http://api.openweathermap.org/data/2.5/weather?lat=" + userLatitude + "&lon=" + userLongitude + "&APPID=" + apiKey;
+    			console.log(openWeatherMapURL);
+            } else if (status == "timeout") {
+                alert ("Connection timeout. Please try again later.");
+            } else if (status == "error" || status == "parseerror") {
+                alert ("System error. Please try again later.");
+            } else {
+                alert ("An error occured");
+            }
         }).then(getUserWeather);
     }
 
     function getUserWeather() {
-        $.getJSON(openWeatherMapURL, function(json) {
-            // Get Temperature & Unit Conversion
-            userKelvinTemp = Math.round(json.main.temp);
-            console.log(userKelvinTemp);
-            userFahrenheitTemp = Math.round(userKelvinTemp * (9 / 5) - 459.67);
-            console.log(userFahrenheitTemp);
-            userCelsiusTemp = Math.round(userKelvinTemp - 273.15);
-            console.log(userCelsiusTemp);
+        $.getJSON(openWeatherMapURL, function(json, status) {
+            if (status == "success") {
+                // Get Temperature & Unit Conversion
+                userKelvinTemp = Math.round(json.main.temp);
+                console.log(userKelvinTemp);
+                userFahrenheitTemp = Math.round(userKelvinTemp * (9 / 5) - 459.67);
+                console.log(userFahrenheitTemp);
+                userCelsiusTemp = Math.round(userKelvinTemp - 273.15);
+                console.log(userCelsiusTemp);
 
-            // Get Weather
-            userWeatherCondition = json.weather[0].main;
-            console.log(userWeatherCondition);
-            userWeatherIcon = json.weather[0].icon;
-            console.log(userWeatherIcon);
-            userWeatherIconLink = "http://openweathermap.org/img/w/" + userWeatherIcon + ".png";
-            console.log(userWeatherIconLink);
+                // Get Weather
+                userWeatherCondition = json.weather[0].main;
+                console.log(userWeatherCondition);
+                userWeatherIcon = json.weather[0].icon;
+                console.log(userWeatherIcon);
+                userWeatherIconLink = "http://openweathermap.org/img/w/" + userWeatherIcon + ".png";
+                console.log(userWeatherIconLink);
+            } else if (status == "timeout") {
+                alert ("Connection timeout. Please try again later.");
+            } else if (status == "error" || status == "parseerror") {
+                alert ("System error. Please try again later.");
+            } else {
+                alert ("An error occured");
+            }
         }).then(changeUserInterface);
     }
 
@@ -60,6 +76,7 @@ $(document).ready(function() {
 
     function toggleTemp() {
         $('#user-temperature').text(isCelcius ? (userFahrenheitTemp + '° F') : (userCelsiusTemp + '° C'));
+        $('#toggle').text(isCelcius ? 'Show Celcius' : 'Show Fahrenheit');
 
         isCelcius = isCelcius ? false : true;
     }
